4cde16a86bb01831a14bcd8016580ca40a595d36
KamranJabiyev <49950252+KamranJabiyev@users.noreply.github.com>
2019-10-27 20:12:16 +0400

    Add files via upload

---
+++ 27.10.2019/js/script.js
@@ -0,0 +1,92 @@
+// var a = 5;
+// var a = 6;
+
+// let a = 5;
+// var a = 6;
+
+// const a = 5;
+// a = 5;
+// console.log(a);
+
+// let name = "Selcan";
+// let age = 18;
+// let city = "Quba";
+
+// if (age > 18 && city == "Baki") {
+//     console.log("Welcome " + city + "-li " + name)
+
+// } else {
+
+//     console.log("Goodbye " + name);
+// }
+
+// let result = age > 18 ? "Welcome" : city == "Baki" ? "Welcome Bakili" : "Welcome Bakisiz"
+// console.log(result)
+
+// let a = 78;
+// let b = 25;
+
+// a += b;
+// b = a - b;
+// a = a - b;
+
+// console.log("a= " + a)
+// console.log("b= " + b)
+
+// for (var i = 1; i <= 10; i++) {
+//     console.log(i);
+// }
+
+// let i = 1;
+
+// while (i <= 10) {
+
+//     console.log(i);
+//     ++i;
+// }
+
+// do {
+//     i++;
+//     console.log(i);
+
+
+// } while (i <= 10)
+
+// let age = 17;
+
+// if (age > 18) {
+//     console.log("Cavansan")
+// } else if (age == 18) {
+//     console.log("Yeniyetmesen")
+// } else {
+//     console.log("Xetrivize deymesin ushaqsan")
+// }
+
+// var name = "Kenan";
+
+// if (1 == 1) {
+//     var name = "Ferid";
+//     console.log(name)
+// }
+
+// console.log(name)
+
+// for (let i = 0; i <= 10; i++) {
+
+// }
+
+// console.log(i)
+
+// let arr = [15, 25, 78, true, "Mubu", "Mubucimbom", "Ferid"]
+
+
+
+// for (let i = 0; i < arr.length; i++) {
+//     console.log(arr[i])
+// }
+
+// for (mubu of arr) {
+//     if (!isNaN(mubu) && mubu !== true) {
+//         console.log(mubu)
+//     }
+// }
